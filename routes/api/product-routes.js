0fbf0968074dcf112697638468423530a2d216e5
Kyro <mariogallardo1331@yahoo.com>
2022-02-10 13:59:39 -0600

    finishing up tag-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,14 +6,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   Product.findAll({
-    
+    include: Category
   })
+  .then((products) => {
+    res.json(products)
+  })
+  .catch(err => res.status(400).json(err));
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((product) => {
+    res.json(product)
+  })
+  .catch(err => res.status(400).json(err));
 });
 
 // create new product
@@ -91,7 +102,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((product) => {
+    res.json(product)
+  })
+  .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
